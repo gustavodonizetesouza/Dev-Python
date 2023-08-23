2b3a846da405f5d44d7f50f65ca504b2fb81f64a
Gustavo Donizete de Souza <48589828+gustavodonizetesouza@users.noreply.github.com>
2023-08-23 19:26:32 -0300

    update aula 17

---
+++ Python-Tkinter/Iniciante/aula17_ModificarStrings.py
@@ -0,0 +1,23 @@
+a =  "Gustavo Donizete de Souza"
+b = a.upper()# modifica o conteudo da string para maiusculo
+
+print(b)
+print("--------------------------------------------------------")
+
+print(a.lower())# modifiica a string para minusculo
+
+print("--------------------------------------------------------")
+
+c = "       Gustavo Donizete       "
+print(">" + c.strip() + "<")# remove os espaços do Inicio e final
+
+print("--------------------------------------------------------")
+
+print(a.replace("u","x"))
+
+print("--------------------------------------------------------")
+
+d = "Carro, Moto, Caminhao"
+print(d.split(","))
+
+print("--------------------------------------------------------")
