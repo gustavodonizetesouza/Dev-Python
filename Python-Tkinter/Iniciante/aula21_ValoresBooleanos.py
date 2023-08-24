fd822920e3201585f3544e122a878198c15906d2
Gustavo Donizete de Souza <48589828+gustavodonizetesouza@users.noreply.github.com>
2023-08-24 11:49:04 -0300

    create aula 21

---
+++ Python-Tkinter/Iniciante/aula21_ValoresBooleanos.py
@@ -0,0 +1,15 @@
+# Literal True ou False
+# Visto em tipos de dados
+# usados em expressões condicionais 
+
+print(10 > 2 )
+print(10 == 1)
+print(10 <2)
+
+a = 200
+b = 500
+
+if a > b:
+    print("A é maior que B")
+else:
+    print(" B é maior que A")
