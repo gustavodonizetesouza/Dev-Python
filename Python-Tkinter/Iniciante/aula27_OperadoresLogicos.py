c3867d687cdbe643448f90d108b4ba0d4df059ae
Gustavo Donizete de Souza <48589828+gustavodonizetesouza@users.noreply.github.com>
2023-08-28 12:55:25 -0300

    create-aula-27OperadoresLogicos

---
+++ Python-Tkinter/Iniciante/aula27_OperadoresLogicos.py
@@ -0,0 +1,6 @@
+x = 5
+print(x > 3 and x < 10) # irá retornar verdadeiro se as duas expressoes retorna verdadeiro. Expressão END
+
+print(x > 3 or x < 4) # irá retornar verdadeiro se as apenas uma das expressoes retorna verdadeiro. Expressão OR
+
+print(not(x > 3 and x < 10)) # Expressão que invert o retorno da experessa. Se retornar verdadeira retorna falso e vice-verso.
