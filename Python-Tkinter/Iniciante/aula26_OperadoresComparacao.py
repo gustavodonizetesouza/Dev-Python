33e55231f6ca58e5220bdb5e0ac5b9499d617650
Gustavo Donizete de Souza <48589828+gustavodonizetesouza@users.noreply.github.com>
2023-08-28 12:44:20 -0300

    Create-Aula26-OperadoresComparacao

---
+++ Python-Tkinter/Iniciante/aula26_OperadoresComparacao.py
@@ -0,0 +1,8 @@
+x = 5
+y = 3
+print(x == y) # Operação valores iguais
+print(x != y) # Operador de Valor x é diferente de y
+print(x > y)  # Operador de valor x é maior que y
+print(x < y)  # Operador de valor x é menor que y
+print(x >= y) # Operador de valor x é maior ou igual que y
+print(x <= y) # Operador de valor x é maior ou igual que y
