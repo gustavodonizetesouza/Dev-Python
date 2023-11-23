ea5954a431a7d2a85db5543d4192af43da985984
Gustavo Donizete de Souza <48589828+gustavodonizetesouza@users.noreply.github.com>
2023-11-22 21:31:12 -0300

    inseri os comentário no código

--- Projetos/clientes.py
+++ Projetos/clientes.py
@@ -10,13 +10,19 @@ class Application():
 
     def tela(self):
         #Titulo do Formulário
-        self.main.title("Cadastro de Clientes")        
+        self.main.title("Cadastro de Clientes")   
+
         #Configuração da cor do fundo do formulário
         self.main.configure(background='lightblue')
+
         #Configuração tamanho inicial do formulário
-        # 1080 largura do formulario na tela e 
+        # 1080 largura do formulario na tela e 550 é altura
         self.main.geometry("1080x550") 
-        #self.main.resizable(False, False)
+
+        # esta permite ´pode ajustar o tamanho do form pelo mouse
+        self.main.resizable(False, False)
+
+        #permite maximizar e minimizar o form em um tamanho maximo e minimo
         #self.main.maxsize(width=900, height=700)
         #self.main.minsize(width=400, height=300)
 
