ab6523b06cf6626c71ee136b28f276df9cd747af
Gustavo Donizete de Souza <48589828+gustavodonizetesouza@users.noreply.github.com>
2023-11-30 13:04:18 -0300

    Ajustes Diretorio

--- Iniciantes/1 - Conceitos fundamentais e primeiros passos/aula04_sintaxe.py
+++
@@ -1,2 +0,0 @@
-if 5 > 2: #não tem chaves como outras linguagens de programação
-    print("5 é maior que 2") #antes do print tem uma tabulação-> identação de 4 espaços
--- Iniciantes/4 - Operadores/aula22_CalculoTriplo.py
+++
@@ -1,6 +0,0 @@
-num = input("Digite uma valor: ")
-
-triplo = int(num) * 3
-print()
-
-print("O Tripo do Valor:", num , "é:" , triplo)
